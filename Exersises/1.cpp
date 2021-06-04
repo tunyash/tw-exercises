9751624884664b648403f7362f6607e8c9660208
elproblema <gribennikvitalij@gmail.com>
2021-06-04 18:14:42 +0300

    First Task Submit

--- Exersises/1.cpp
+++ Exersises/1.cpp
@@ -1,27 +1,99 @@
-﻿// Exersises.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <string>
+#include <vector>   
+#include <random>
+#include <algorithm>
+#include <time.h>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/topological_sort.hpp>
+#include <boost/graph/depth_first_search.hpp>
 
 using namespace std;
 using namespace boost;
 
+class disjoint_path_finder {
+
+private:
+    struct Vertex {
+        int id = -1;
+    };
+
+    typedef adjacency_list<vecS, vecS, undirectedS,
+        Vertex, no_property> Graph;
+    typedef graph_traits<Graph>::vertex_descriptor vertex_t;
+    typedef graph_traits<Graph>::edge_descriptor edge_t;
+
+    vector<int> color;
+    vector<pair<int, int>> pairs;
+    Graph g;
+    
+    vertex_t get_vertex_by_number(int i, Graph& g) {
+        return (*(vertices(g).first + i));
+    }
+
+    bool my_dfs(vertex_t start, vertex_t end, int c, Graph& g) { 
+        // returns true if path from s_i to t_i was found
+        // else false
+        color[g[start].id] = c;
+        if (start == end) return true;
+        int size = adjacent_vertices(start, g).second - adjacent_vertices(start, g).first;
+        vector<int> go(size);
+        for (int i = 0; i < go.size(); ++i) {
+            go[i] = i;
+        }
+        random_shuffle(go.begin(), go.end());
+        for (int i = 0; i < go.size(); ++i) {
+            auto u = *(adjacent_vertices(start, g).first + go[i]);
+            if ((color[g[u].id] == -1 || color[g[u].id] == c) && my_dfs(u, end, c, g)) return true;
+        }
+        color[g[start].id] = -1;
+        return false;
+    }
+
+public:
+
+    disjoint_path_finder(int n, vector<pair<int, int>> edges, vector<pair<int, int>> pairs) : pairs(pairs) {
+        srand(time(0));
+        color.assign(n, -1);
+        g = Graph(edges.begin(), edges.end(), n);
+        int i = 0;
+        for (auto j = vertices(g).first; j != vertices(g).second; ++j) {
+            g[*j].id = i;
+            ++i;
+        }
+    }
+
+    pair<bool, vector<int>> solve() {
+        for (int tries = 0; tries < 10; ++tries) {
+            color.assign(color.size(), -1);
+            for (int i = 0; i < pairs.size(); ++i) {
+                color[pairs[i].first] = i;
+                color[pairs[i].second] = i;
+            }
+            bool ok = 1;
+            for (int i = 0; i < pairs.size() && ok; ++i) {
+                if (!my_dfs(get_vertex_by_number(pairs[i].first, g),
+                    get_vertex_by_number(pairs[i].second, g),
+                    i, g)) ok = 0;
+            }
+            if (ok && find(color.begin(), color.end(), -1) == color.end()) {
+                return make_pair(true, color);
+            }
+            return make_pair(false, vector<int>(color.size(), -1));
+        } 
+    }
+};
+
+
+
 int main()
-{
-    cout << "Hello World!\n";
+{ 
+    vector<pair<int, int>> edges = {
+        {0, 1}, {1, 2}, {2, 3}, {3, 0}
+    };
+    vector<pair<int, int>> pairs = {
+        {0, 1}, {2, 3}
+    };
+    disjoint_path_finder ans(4, edges, pairs);
+    auto answer = ans.solve();
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
