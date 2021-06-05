67aa55fce732c18b1b19db9a80843948e86d5fbd
elproblema <gribennikvitalij@gmail.com>
2021-06-05 15:07:34 +0300

    Docstrings commit

--- Exersises/1.h
+++ Exersises/1.h
@@ -1,5 +1,4 @@
 ﻿#include <iostream>
-#include <string>
 #include <vector>   
 #include <random>
 #include <algorithm>
@@ -8,78 +7,90 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/depth_first_search.hpp>
 
-using namespace std;
-using namespace boost;
-
 class disjoint_path_finder {
 
 private:
-    struct Vertex {
-        int id = -1;
-    };
 
-    typedef adjacency_list<vecS, vecS, undirectedS,
-        Vertex, no_property> Graph;
-    typedef graph_traits<Graph>::vertex_descriptor vertex_t;
-    typedef graph_traits<Graph>::edge_descriptor edge_t;
+    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> Graph;
+    typedef boost::graph_traits<Graph>::vertex_descriptor vertex_t;
+    typedef boost::graph_traits<Graph>::edge_descriptor edge_t;
 
-    vector<int> color;
-    vector<pair<int, int>> pairs;
-    Graph g;
+    std::vector<int> _color;
+    std::vector<std::pair<int, int>> _pairs;
+    Graph _g;
     
     vertex_t get_vertex_by_number(int i, Graph& g) {
+        /*
+        Transform number of vertex |i| in Graph |g| into vertex_t 
+        @param Order number of vertex in the Graph |g|
+        @return vertex_t in the Graph |g|
+        */
         return (*(vertices(g).first + i));
     }
 
     bool my_dfs(vertex_t start, vertex_t end, int c, Graph& g) { 
-        // returns true if path from s_i to t_i was found
-        // else false
-        color[g[start].id] = c;
+/*
+        Finds a path from |start| to |end| in |g| while enumerating the neighbors of each vertex in an order chosen at random. 
+        Marks all the nodes along the found path in the |this->_color| vector with |c|.
+
+        @param start is the start node of the potential path
+        @param end is the end node of the potential path
+        @param c is the _color to mark the nodes in the path with
+        @param g the graph to make the traversal
+        @return true if a path was found and false otherwise
+*/
+        _color[start] = c;
         if (start == end) return true;
-        int size = adjacent_vertices(start, g).second - adjacent_vertices(start, g).first;
-        vector<int> go(size);
+        std::vector<int> go(degree(start, g));
         for (int i = 0; i < go.size(); ++i) {
             go[i] = i;
         }
         random_shuffle(go.begin(), go.end());
-        for (int i = 0; i < go.size(); ++i) {
-            auto u = *(adjacent_vertices(start, g).first + go[i]);
-            if ((color[g[u].id] == -1 || color[g[u].id] == c) && my_dfs(u, end, c, g)) return true;
+        for (int i : go) {
+            auto u = *(adjacent_vertices(start, g).first + i);
+            if ((_color[u] == -1 || _color[u] == c) && my_dfs(u, end, c, g)) return true;
         }
-        color[g[start].id] = -1;
+        _color[start] = -1;
         return false;
     }
 
 public:
 
-    disjoint_path_finder(int n, vector<pair<int, int>> edges, vector<pair<int, int>> pairs) : pairs(pairs) {
+    disjoint_path_finder(int n, std::vector<std::pair<int, int>> edges, std::vector<std::pair<int, int>> pairs) : _pairs(pairs) {
+        /*
+        Constructor generates Graph |_g| with |n| vertices and edges from |edges|
+        @param |n| amount of verices
+        @param |edges| - edges of the Graph |_g|
+        @param |_pairs| - pairs of vertices that would form disjoint path decomposition 
+        */
         srand(time(0));
-        color.assign(n, -1);
-        g = Graph(edges.begin(), edges.end(), n);
-        int i = 0;
-        for (auto j = vertices(g).first; j != vertices(g).second; ++j) {
-            g[*j].id = i;
-            ++i;
-        }
+        _color.assign(n, -1);
+        _g = Graph(edges.begin(), edges.end(), n);
     }
 
-    pair<bool, vector<int>> solve() {
-        for (int tries = 0; tries < 10; ++tries) {
-            color.assign(color.size(), -1);
-            for (int i = 0; i < pairs.size(); ++i) {
-                color[pairs[i].first] = i;
-                color[pairs[i].second] = i;
+    std::pair<bool, std::vector<int>> solve(int tries = 100) {
+        /*
+        Function tries |tries| times to find disjoint path decomposition of the graph |_g|
+        @param |tries| is amount of tries
+        @return pair.first is true if disjoint path decomposition was found else false 
+        @return pair.second is coloring of graph |_g| where |color[v] == c| means that vertex |v| belongs to path from |_pairs[c].first| to |_pairs[c].second| 
+        */
+        for (int t = 0; t < tries; ++t) {
+            _color.assign(_color.size(), -1);
+            for (int i = 0; i < _pairs.size(); ++i) {
+                _color[_pairs[i].first] = i;
+                _color[_pairs[i].second] = i;
             }
             bool ok = 1;
-            for (int i = 0; i < pairs.size() && ok; ++i) {
-                if (!my_dfs(get_vertex_by_number(pairs[i].first, g),
-                    get_vertex_by_number(pairs[i].second, g),
-                    i, g)) ok = 0;
+            for (int i = 0; i < _pairs.size() && ok; ++i) {
+                if (!my_dfs(get_vertex_by_number(_pairs[i].first, _g),
+                    get_vertex_by_number(_pairs[i].second, _g),
+                    i, _g)) ok = 0;
             }
-            if (ok && find(color.begin(), color.end(), -1) == color.end()) {
-                return make_pair(true, color);
+            if (ok) {
+                return make_pair(true, _color);
             }
-            return make_pair(false, vector<int>(color.size(), -1));
+            return std::make_pair(false, std::vector<int>(_color.size(), -1));
         } 
     }
 };
--- Exersises/test1.cpp
+++ Exersises/test1.cpp
@@ -13,8 +13,19 @@ TEST_CASE("simple test") {
     vector<pair<int, int>> pairs = {
         {0, 1}, {2, 3}
     };
-    disjoint_path_finder q(4, edges, pairs);
-    CHECK(q.solve().first);
+    disjoint_path_finder q(/*n=*/4, edges, pairs);
+    CHECK(q.solve(/*tries=*/30).first);
+}
+
+TEST_CASE("another simple test") {
+    vector<pair<int, int>> edges = {
+        {0, 1}, {2, 3}
+    };
+    vector<pair<int, int>> pairs = {
+        {0, 3}, {1, 2}
+    };
+    disjoint_path_finder q(/*n=*/4, edges, pairs);
+    CHECK(q.solve(/*tries=*/30).first == false);
 }
 
 TEST_CASE("exceptions") {
@@ -27,11 +38,11 @@ TEST_CASE("exceptions") {
     bool pass = 0;
     try {
         pass = 1;
-        disjoint_path_finder q(-3, edges, pairs);
-        q.solve();
+        disjoint_path_finder q(/*n=*/-3, edges, pairs);
+        q.solve(/*tries=*/30);
     }
     catch (...) {
         pass = 0;
     }
-    CHECK(pass == 0);
+    CHECK(pass == false);
 }
