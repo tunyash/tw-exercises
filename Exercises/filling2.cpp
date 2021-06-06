3da8797dc7f45d1ae1da58f1e0157d94baf5adbf
elproblema <gribennikvitalij@gmail.com>
2021-06-06 17:56:07 +0300

    Second Task Commit

---
+++ Exercises/filling2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>   
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <algorithm>
+#include "2.h"
+
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> Graph;
+
+bool PathDecomposition::Check() {
+	int n = boost::num_vertices(_g);
+
+	for (std::vector<int> bug : _path) {
+		std::sort(bug.begin(), bug.end());
+		bug.resize(std::unique(bug.begin(), bug.end()) - bug.begin());
+		//Erasing non-unique vertices from bugs
+		for (int v : bug) {
+			if (v < 0 && v >= n)
+				throw PathDecomposition::CorectnessException("Vertices in bugs are not in [0; |V(_g)|)");
+		}
+	}
+
+	std::vector<int> sum(n);
+	std::vector<int> left(n, n + 1);
+	std::vector<int> right(n, -1);
+	
+	for (int i = 0; i < _path.size(); ++i) {
+		for (int v : _path[i]) {
+			sum[v]++;
+			left[v] = std::min(left[v], i);
+			right[v] = std::max(right[v], i);
+		}
+	}
+
+	for (int i = 0; i < n; ++i) {
+		if (sum[i] == 0) throw PathDecomposition::CorectnessException("Not all vertices are in path-width decomposition");
+		if (right[i] - left[i] + 1 != sum[i]) throw PathDecomposition::CorectnessException("In Decomposition exists vertex u and i < j < k such that bug[i] and bug[k] contains u but bug[j] doesn't");
+	}
+
+	Graph path_g(n);
+	
+	for (std::vector<int> bug : _path) {
+		for (int v : bug) {
+			for (int u : bug) {
+				if (u == v) continue;
+				boost::add_edge(u, v, path_g);
+			}
+		}
+	}
+
+	for (int v = 0; v < n; ++v) {
+		for (auto it = boost::adjacent_vertices(v, _g).first; it != boost::adjacent_vertices(v, _g).second; ++it) {
+			if (std::find(boost::adjacent_vertices(v, path_g).first, boost::adjacent_vertices(v, path_g).second, *it) ==
+				boost::adjacent_vertices(v, path_g).second) throw PathDecomposition::CorectnessException("Not all edges are in path-width decomposition");
+		}
+	}
+}
+
+PathDecomposition::PathDecomposition(std::vector<std::vector<int>> path, Graph g): _path(path), _g(g) {
+	Check();
+}
---
+++ Exercises/test.cpp
@@ -0,0 +1,141 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <iostream>
+#include <vector>
+#include "1.h"
+#include "2.h"
+#include "doctest.h"
+
+using namespace std;
+using namespace boost;
+
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> Graph;
+
+TEST_CASE("simple test") {
+    vector<pair<int, int>> edges = {
+        {0, 1}, {1, 2}, {2, 3}, {3, 0}
+    };
+    vector<pair<int, int>> pairs = {
+        {0, 1}, {2, 3}
+    };
+    disjoint_path_finder q(/*n=*/4, edges, pairs);
+    CHECK(q.solve(/*tries=*/30).first);
+}
+
+TEST_CASE("disconnected graph test") {
+    vector<pair<int, int>> edges = {
+        {0, 1}, {2, 3}
+    };
+    vector<pair<int, int>> pairs = {
+        {0, 3}, {1, 2}
+    };
+    disjoint_path_finder q(/*n=*/4, edges, pairs);
+    CHECK(q.solve(/*tries=*/30).first == false);
+}
+
+TEST_CASE("exceptions") {
+    vector<pair<int, int>> edges = {
+        {-1, 0}
+    };
+    vector<pair<int, int>> pairs = {
+        {0, 1}, {2, 3}
+    };
+    bool pass = 0;
+    try {
+        pass = 1;
+        disjoint_path_finder q(/*n=*/-3, edges, pairs);
+        q.solve(/*tries=*/30);
+    }
+    catch (...) {
+        pass = 0;
+    }
+    CHECK(pass == false);
+}
+
+TEST_CASE("Correct path-width decomposition") {
+    Graph g(/*n=*/4);
+    add_edge(0, 1, g);
+    add_edge(0, 2, g);
+    add_edge(3, 1, g);
+    add_edge(3, 2, g);
+
+    bool pass = 1;
+    try {
+        PathDecomposition({ {0, 1, 2, 3} }, g);
+    }
+    catch (...) {
+        pass = 0;
+    }
+    CHECK(pass);
+}
+
+TEST_CASE("Not all vertices") {
+    Graph g(/*n=*/4);
+    add_edge(0, 1, g);
+    add_edge(0, 2, g);
+    add_edge(3, 1, g);
+    add_edge(3, 2, g);
+
+    bool pass = 0;
+    try {
+        PathDecomposition({ {0, 1, 2} }, g);
+    }
+    catch (PathDecomposition::CorectnessException ex) {
+        pass = 1;
+        cout << ex.what() << "\n";
+    }
+    CHECK(pass);
+}
+
+TEST_CASE("Not all edges") {
+    Graph g(/*n=*/4);
+    add_edge(0, 1, g);
+    add_edge(0, 2, g);
+    add_edge(3, 1, g);
+    add_edge(3, 2, g);
+
+    bool pass = 0;
+    try {
+        PathDecomposition({ {0}, {1}, {2}, {3} }, g);
+    }
+    catch (PathDecomposition::CorectnessException ex) {
+        pass = 1;
+        cout << ex.what() << "\n";
+    }
+    CHECK(pass);
+}
+
+TEST_CASE("Not all edges") {
+    Graph g(/*n=*/4);
+    add_edge(0, 1, g);
+    add_edge(0, 2, g);
+    add_edge(3, 1, g);
+    add_edge(3, 2, g);
+
+    bool pass = 0;
+    try {
+        PathDecomposition({ {0}, {1}, {2}, {3} }, g);
+    }
+    catch (PathDecomposition::CorectnessException ex) {
+        pass = 1;
+        cout << ex.what() << "\n";
+    }
+    CHECK(pass);
+}
+
+TEST_CASE("Third rule exception") {
+    Graph g(/*n=*/4);
+    add_edge(0, 1, g);
+    add_edge(0, 2, g);
+    add_edge(3, 1, g);
+    add_edge(3, 2, g);
+
+    bool pass = 0;
+    try {
+        PathDecomposition({ {0, 1}, {1, 2}, {2, 3}, {3, 0} }, g);
+    }
+    catch (PathDecomposition::CorectnessException ex) {
+        pass = 1;
+        cout << ex.what() << "\n";
+    }
+    CHECK(pass);
+}
